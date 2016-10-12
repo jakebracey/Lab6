14750a5452728b613269ab38e0ffddc4109a52d9
ZShikles <ztskd2@mail.missouri.edu>
2016-10-12 15:04:43 -0500

    Comments added

--- Lab6-ex8.c
+++ Lab6-ex8.c
@@ -4,16 +4,16 @@
 
 int main(void)
 {
-	char s[25];
+	char s[25]; //changed s to array notation to staticly allocated space
 	char *dyn_s;
 	int ln;
 	printf("Enter the input string\n");
 	scanf("%s",s);
 	ln = strlen(s);
-	dyn_s = (char*)malloc(strlen(s)+1);
+	dyn_s = (char*)malloc(strlen(s)+1); //removed "*" behind dyn_s. you shouldn't dereference the variable you're trying to malloc
 	dyn_s = s;
 	dyn_s[strlen(s)]='\0';
-	printf("%s\n", dyn_s);
+	printf("%s\n", dyn_s); //changed printf formatting due to warnings
 	return 0;
 }
 
