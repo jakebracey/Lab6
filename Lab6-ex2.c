01e0ec8c1346cc0caea857efb0f59ce490f86581
ZShikles <ztskd2@mail.missouri.edu>
2016-10-12 14:55:58 -0500

    Comments added

--- Lab6-ex2.c
+++ Lab6-ex2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-    char p[7] = "Mizzou";
+    char p[7] = "Mizzou"; //changed to array notation, you can't directly set a string to a pointer
     p[ 3 ] = 'P';
-    printf("%s\n",p);
+    printf("%s\n",p); // added \n for formating convenience
 }
