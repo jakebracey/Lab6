4a6a91835123edbbe0770d50689be49022f84549
ZShikles <ztskd2@mail.missouri.edu>
2016-10-12 14:57:21 -0500

    Update Lab6-ex4.c

--- Lab6-ex4.c
+++ Lab6-ex4.c
@@ -4,7 +4,7 @@
 void main()
 {
     int i=0;
-    char arr1[ 20 ] = "Trumantiger";
+    char arr1[ 20 ] = "Trumantiger"; //increased the number of elements in arr1 so that strcpy doesn't cause seg fault
     char arr2[ 20 ];
     while(i<20){
         arr2[ i ] = 'A';
